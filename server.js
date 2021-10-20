0ed3511e543c594bd3dda92c471f318fdeb2fc86
santiak3 <71164211+santiak3@users.noreply.github.com>
2021-10-20 19:10:30 +0100

    Added user registration

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import User from './models/User';
 
 // Initialize express application
 const app = express();
@@ -11,16 +13,22 @@ connectDatabase();
 
 // Configure Middleware
 app.use(express.json({ extended: false}));
-app.use(cors({
+app.use(
+    cors({
         origin: 'http://localhost:3000'
-    }));
+    })
+);
 
 //API endpoints
 /**
  * @route GET /
  * @desc Test endpoint
  */
-app.get('/', (req,res) => res.send('http get request sent to root api endpoint'));
+app.get('/', (req,res) => 
+    res.send('http get request sent to root api endpoint')
+    );
+
+app.get('/api/', (req, res) => res.send('http get request sent to api'));
 
 /**
  * @route POST api/users
@@ -29,16 +37,47 @@ app.get('/', (req,res) => res.send('http get request sent to root api endpoint')
 app.post(
     '/api/users',
     [
-        check('name', 'Please enter your name').not().isEmpty(),
+        check('name', 'Please enter your name')
+            .not()
+            .isEmpty(),
         check('email', 'Please enter a valid email').isEmail(),
-        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+            ).isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
-        } else { 
-          return res.send(req.body);
+        } else {
+            const { name, email, password } = req.body;
+            try {
+                // Check if user exists
+                let user = await User.findOne({ email: email });
+                if (user) {
+                    return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'User already exists' }] });
+                }
+
+                // Create a new user
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                // Encrypt the password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                // Save to the db and return
+                await user.save();
+                res.send('User successfully registered');
+            } catch (error) {
+              res.status(500).send('Server error');
+            }
         }
     }
 );
@@ -46,5 +85,3 @@ app.post(
 // Connection listener
 const port = 5000;
 app.listen(port, () => console.log('Express server running on port',port));
-
-
